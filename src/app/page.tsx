b5911bac871d0b42f7d7806c28b1a2602d8d65ad
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-04-19 04:59:10 +0530

    adding the experience timeline component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Experience from "@/components/Experience";
 import TypewriterOverrideEffect from "@/components/TypewriterOverrideEffect";
 
 export default function Home() {
   return(
     <div>
       <TypewriterOverrideEffect/>
+      <Experience/>
     </div>
   )
 }
