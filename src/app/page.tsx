83babd9f79d85a2d1eb20b5803b526f307c607f9
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-28 15:44:07 +0530

    adding the timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import Banner from '@/components/Banner';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 import { Footer } from '@/components/Footer';
+import SkillsSection from '@/components/SkillSection';
+import Experience from '@/components/Experience';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <SkillsSection />
+      <Experience />
       {/* <Skills /> */}
       <Projects />
       <Contact />
