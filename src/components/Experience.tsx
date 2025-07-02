423c330abdaf2b653ccdf179deaeda0eccadd712
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-02 19:29:33 +0530

    starting with experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,6 +4,31 @@ import { VerticalTimeline }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import TimelineElement from './TimelineElement';
 
+const data = [
+  {
+    position: "Online Tutor",
+    company: "My Engineering Buddy",
+    role: "Freelance",
+    type: "Remote",
+    Description:
+        `
+          Tutoring engineering students in subjects like Data Structures, Algorithms, Web Development, Database Management, and Computer Science Fundamentals.
+
+          Delivering personalized 1-on-1 online sessions, simplifying complex concepts, and helping students achieve academic success.
+
+          Designing practice problems and walkthroughs tailored to individual learning styles and syllabi.
+
+          Assisting students with project guidance, debugging, and exam preparation, fostering strong conceptual foundations.
+
+          Leveraging tools like Google Meet, Zoom, shared coding platforms, and digital whiteboards for real-time interactive teaching.
+
+          Maintaining excellent student satisfaction ratings through engaging teaching and timely support.
+        `,
+    duration: "Jan 2023 - Present",
+    country: "India",
+  }
+]
+
 const Experience = () => {
   return (
     <section id='experience'style={{backgroundImage: 'radial-gradient(circle, #0D3E85 -90%, #021024 100%)'}}>
